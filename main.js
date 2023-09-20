fa01993d35d398c4b4c95a2c1fb457799c4bc7bd
Nguyen Hoang Minh <nghgminh163@gmail.com>
2023-09-20 10:42:35 +0700

    feat: add server with get list and detail student

---
+++ main.js
@@ -0,0 +1,17 @@
+const app = require('express')();
+const data = require('./students.json');
+app.listen(3000, () => console.log("Server is running"));
+
+app.get('/api/students', (req, res) => {
+    res.json(data);
+})
+
+app.get('/api/students/:id', (req, res) => {
+    const id = req.params.id;
+    const student = data.find(student => student.id === parseInt(id));
+    if(student){
+        res.json(student);
+    } else {
+        res.status(404).send("Not found")
+    }
+})
