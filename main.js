02dc3e0c49d295eccd86aa6c7197a81234123253
Nguyen Hoang Minh <nghgminh163@gmail.com>
2023-09-25 09:41:14 +0700

    feat: update backend

--- main.js
+++ main.js
@@ -3,13 +3,17 @@ const data = require('./students.json');
 app.listen(3000, () => console.log("Server is running"));
 
 app.get('/api/students', (req, res) => {
-    res.json(data);
+    res.json(data.filter(v => ({
+        id: v.id,
+        name: `${v.first_name} ${v.last_name}`,
+    }))
+    );
 })
 
 app.get('/api/students/:id', (req, res) => {
     const id = req.params.id;
     const student = data.find(student => student.id === parseInt(id));
-    if(student){
+    if (student) {
         res.json(student);
     } else {
         res.status(404).send("Not found")
